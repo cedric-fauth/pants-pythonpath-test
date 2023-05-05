ac8b51ee431c4cec9af27bec053879490cfd52e2
Cedric Fauth <cedric.fauth@nefino.de>
2023-05-05 09:04:42 +0000

    added readme and lib

--- src/script.py
+++ src/script.py
@@ -2,4 +2,9 @@ import os
 
 print(os.getenv("hello"))
 print(os.getenv("PYTHONPATH"))
-print("Hello World")
+
+print("Now we call the lib")
+
+# just an example the lib could be anywhere on the system but can only be imported if PYTHONPATH is set correctly
+import lib
+
